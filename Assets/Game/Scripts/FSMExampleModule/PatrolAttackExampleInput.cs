dc779b997420b3dffff3b94c10d471d3d11ed98e
Vladislav <GimliVlad@list.ru>
2026-03-21 20:09:08 +0300

    Update example

---
+++ Assets/Game/Scripts/FSMExampleModule/PatrolAttackExampleInput.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using FSMModule.Graph;
+
+namespace FSMModule.Examples
+{
+    [AddComponentMenu("FSM Examples/Patrol Attack Example Input")]
+    public sealed class PatrolAttackExampleInput : MonoBehaviour
+    {
+        [SerializeField] private FsmGraphRunner runner;
+        [SerializeField] private string attackKey = PatrolAttackExampleKeys.ShouldAttack;
+        [SerializeField] private KeyCode toggleAttackKey = KeyCode.Space;
+        [SerializeField] private KeyCode forcePatrolKey = KeyCode.Alpha1;
+        [SerializeField] private KeyCode forceAttackKey = KeyCode.Alpha2;
+
+        private void Reset() => runner = GetComponent<FsmGraphRunner>();
+
+        private void Update()
+        {
+            if (runner == null || runner.RuntimeContext == null)
+                return;
+
+            if (Input.GetKeyDown(toggleAttackKey))
+                SetAttackEnabled(!IsAttackEnabled());
+
+            if (Input.GetKeyDown(forcePatrolKey))
+                SetAttackEnabled(false);
+
+            if (Input.GetKeyDown(forceAttackKey))
+                SetAttackEnabled(true);
+        }
+
+        private bool IsAttackEnabled() =>
+            runner.RuntimeContext.Blackboard.TryGetValue(attackKey, out bool value) && value;
+
+        private void SetAttackEnabled(bool value) =>
+            runner.RuntimeContext.Blackboard.SetValue(attackKey, value);
+    }
+}
